713e1aded4acc865da3ed5ac9be04912e8268335
masterdemon-adi <78732670+masterdemon-adi@users.noreply.github.com>
2021-04-12 17:18:53 +0530

    Add files via upload

---
+++ Boggie.js
@@ -0,0 +1,24 @@
+class Boggie{
+    constructor(x,y,width,height){
+      var options ={ isStatic: false}
+      
+      this.body =  Bodies.rectangle(x,y,width,height,options);
+
+    this.image = loadImage("images/coach.png")
+
+    Matter.Body.setMass(this.body, this.body.mass*5)
+
+
+      World.add(myWorld,this.body);
+      this.width = width;
+      this.height = height;
+    }
+    show()
+    {var pos = this.body.position
+        imageMode(CENTER);
+        image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
+    }
+  
+  
+  }
+  
